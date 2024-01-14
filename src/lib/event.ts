6ea2d044cc0efe4438d1c6c9bb4b7abeb3a08d31
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2024-01-14 19:58:50 +0200

    feat: New events (#14)

--- src/lib/event.ts
+++ src/lib/event.ts
@@ -1,8 +1,8 @@
-import UIBuilder from "./builder";
-
 enum ServerEventType {
     update = "update",
     updateLayout = "updateLayout",
+    navigation = "navigation",
+    openUrl = "openUrl",
 }
 
 export abstract class ServerEvent {
@@ -27,4 +27,42 @@ export class LayoutUpdateEvent extends ServerEvent {
         super();
         this.layout = layout;
     }
+}
+
+export class NavigationEvent extends ServerEvent {
+    type = ServerEventType.navigation as const;
+    path: string;
+    extra?: Record<string, any>;
+
+    constructor(path: string, extra?: Record<string, any>) {
+        super();
+        this.path = path;
+        this.extra = extra;
+    }
+}
+
+export class OpenUrlEvent extends ServerEvent {
+    type = ServerEventType.openUrl as const;
+    url: string;
+
+    constructor(url: string) {
+        super();
+        this.url = url;
+    }
+}
+
+export const CreateUpdateEvent = (updates: Record<string, any>) => {
+    return new UpdateEvent(updates);
+}
+
+export const CreateLayoutUpdateEvent = (layout: string) => {
+    return new LayoutUpdateEvent(layout);
+}
+
+export const CreateNavigationEvent = (path: string, extra?: Record<string, any>) => {
+    return new NavigationEvent(path, extra);
+}
+
+export const CreateOpenUrlEvent = (url: string) => {
+    return new OpenUrlEvent(url);
 }

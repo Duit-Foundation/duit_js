bcdc34fde2f38e17f919f862d775a943558cb5bc
lesleysin <nikandsin@gmail.com>
2024-02-10 14:49:55 +0200

    Added missing element types

--- src/lib/element.ts
+++ src/lib/element.ts
@@ -1,6 +1,7 @@
-import { CenterUiElement, CheckBoxUiElement, ColoredBoxUiElement, ColumnUiElement, ContainerUiElement, DecoratedBoxUiElement, ExpandedUiElement, GestureDetectorUiElement, ImageUiElement, LifecycleEventListenerUiElement, PaddingUiElement, PositionedUiElement, RichTextUiElement, RowUiElement, ComponentUiElement, SizedBoxUiElement, StackUiElement, TextFieldUiElement, TextUiElement, WrapUiElement, RadioUiElement, RadioGroupContextUiElement, SingleChildScrollViewUiElement, OpacityUiElement, IgnorePoinerUiElement, SliderUiElement } from "../widget_models";
+import { CenterUiElement, CheckBoxUiElement, ColoredBoxUiElement, ColumnUiElement, ContainerUiElement, DecoratedBoxUiElement, ExpandedUiElement, GestureDetectorUiElement, ImageUiElement, LifecycleEventListenerUiElement, PaddingUiElement, PositionedUiElement, RichTextUiElement, RowUiElement, ComponentUiElement, SizedBoxUiElement, StackUiElement, TextFieldUiElement, TextUiElement, WrapUiElement, RadioUiElement, RadioGroupContextUiElement, SingleChildScrollViewUiElement, OpacityUiElement, IgnorePoinerUiElement, SliderUiElement, CustomSingleChildWidget, CustomMultiChildWidget, AlignUiElement, TransformUiElement } from "../widget_models";
+import { DuitLeafElement } from "../widget_models/child";
 
-export type DuitLayoutElement =
+export type DuitLayoutElement<T = any> =
     ColumnUiElement
     | RowUiElement
     | ColoredBoxUiElement
@@ -19,14 +20,20 @@ export type DuitLayoutElement =
     | SingleChildScrollViewUiElement
     | OpacityUiElement
     | IgnorePoinerUiElement
+    | CustomSingleChildWidget<T>
+    | CustomMultiChildWidget<T>
+    | AlignUiElement
+    | TransformUiElement
     | RadioGroupContextUiElement;
 
 export type DuitElement =
     TextUiElement
+    | DuitLeafElement
     | RadioUiElement
     | SliderUiElement
     | CheckBoxUiElement
     | TextFieldUiElement
     | ImageUiElement
     | RichTextUiElement
+    | ComponentUiElement
     | DuitLayoutElement;

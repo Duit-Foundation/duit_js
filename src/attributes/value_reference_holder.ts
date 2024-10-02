a94aad5affff9935491d1b60d1874859dcc5e9b6
lesleysin <nikandsin@gmail.com>
2024-10-02 15:08:17 +0200

    Upd ValuerReference model

--- src/attributes/value_reference_holder.ts
+++ src/attributes/value_reference_holder.ts
@@ -7,6 +7,11 @@ interface ValueRef<T> {
      * The key by which the interpreter will look for the value in the resulting object.
      */
     objectKey: string;
+
+    /**
+     * Default value, used when the value is not found at data object
+     */
+    defaultValue: any;
 }
 
 export interface ValueReferenceHolder<T> {
@@ -17,10 +22,11 @@ export interface ValueReferenceHolder<T> {
  * Creates a new reference from an existing values.
  * @return {ValueRef<T>} The newly created reference.
  */
-export const Ref = <T>(attributeKey: keyof T, objectKey: string): ValueRef<T> => {
+export const Ref = <T>(attributeKey: keyof T, objectKey: string, defaultValue?: any): ValueRef<T> => {
     const ref: ValueRef<T> = {
         attributeKey: attributeKey,
         objectKey: objectKey,
+        defaultValue: defaultValue,
     }
     return ref;
 }

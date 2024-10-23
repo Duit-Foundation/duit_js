d1f3144a39b4d2c2f4b53dac6959dc7f9643f47d
lesleysin <nikandsin@gmail.com>
2024-10-23 12:43:21 +0200

    Added child property to subtree widget model

--- src/widgets/subtree.widget.ts
+++ src/widgets/subtree.widget.ts
@@ -3,6 +3,6 @@ import { SubtreeUiElement } from "../widget_models";
 import type { DuitElement } from "../lib/element";
 import type { BaseProps } from "../widget_models/props";
 
-export const Subtree = (props: BaseProps<DuitElement>): SubtreeUiElement => {
-    return new SubtreeUiElement(props.attributes, props.id);
+export const Subtree = (props: BaseProps<DuitElement>, child?: DuitElement): SubtreeUiElement => {
+    return new SubtreeUiElement(props.attributes, props.id, child);
 }

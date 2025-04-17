1ce8b2344bab88d9546e312ac1c024fe10df8baf
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-17 22:01:21 +0200

    wip

--- src/widgets/animated_rotation.widget.ts
+++ src/widgets/animated_rotation.widget.ts
@@ -4,6 +4,20 @@ import type { AnimatedRotationAttributes } from "../attributes";
 import type { DuitElement } from "../lib/element";
 import type { BaseProps } from "../widget_models/props";
 
+/**
+ * Example usage:
+ * 
+ * AnimatedRotation(
+ *     {
+ *         id: "id",
+ *         attributes: {
+ *             turns: 3.14,
+ *             duration: 1000,
+ *         },
+ *     },
+ *     // child
+ * )
+ */
 export const AnimatedRotation = (props: BaseProps<AnimatedRotationAttributes>, child?: DuitElement): AnimatedRotationUiElement => {
     return new AnimatedRotationUiElement(props.attributes, props.id, child);
 }

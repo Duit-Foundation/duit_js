260d46550d759adc0a534d6950f918e1fa259514
Mr. KrY4k <nikstas056@gmail.com>
2025-06-03 19:53:35 +0500

    remove AnimatedPropertyOwner

--- src/attributes/offstage_attributes.ts
+++ src/attributes/offstage_attributes.ts
@@ -1,6 +1,5 @@
-import { AnimatedPropertyOwner } from "../animations";
 import { ValueReferenceHolder } from "./value_reference_holder";
 
-export interface OffstageAttributes extends ValueReferenceHolder<OffstageAttributes>, AnimatedPropertyOwner {
+export interface OffstageAttributes extends ValueReferenceHolder<OffstageAttributes> {
     offstage?: boolean
 }

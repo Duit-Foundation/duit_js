5f66e5f6357ae06b0711c6a2e5fc25109827a392
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-08-06 17:18:42 +0200

    feat: added ExecutionOptions for all action types (#133)

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -15,6 +15,11 @@ interface Script {
     meta?: Record<string, any>;
 }
 
+interface ExecutionOptions {
+    modifier: "throttle" | "debounce";
+    duration: number;
+}
+
 /**
  * Represents an actions dependency.
  * @interface ActionDependency
@@ -34,6 +39,7 @@ interface HttpActionMetainfo {
  */
 export class BaseAction {
     dependsOn: ActionDependency[];
+    executionOptions?: ExecutionOptions;
 
     /**
      * Url (for http) of event name (for ws)
@@ -46,9 +52,10 @@ export class BaseAction {
     * @param {string} event - The name of the event associated with the action.
     * @param {ActionDependency[]} dependsOn - An array of action dependencies.
     */
-    constructor(event: string, dependsOn: ActionDependency[] = []) {
+    constructor(event: string, dependsOn: ActionDependency[] = [], executionOptions?: ExecutionOptions) {
         this.event = event;
         this.dependsOn = dependsOn;
+        this.executionOptions = executionOptions;
     }
 }
 
@@ -62,8 +69,8 @@ class _HttpAction extends BaseAction {
     meta: HttpActionMetainfo;
     executionType = ExecutionType.transport as const;
 
-    constructor(event: string, meta: HttpActionMetainfo, dependsOn: ActionDependency[] = []) {
-        super(event, dependsOn);
+    constructor(event: string, meta: HttpActionMetainfo, dependsOn: ActionDependency[] = [], executionOptions?: ExecutionOptions) {
+        super(event, dependsOn, executionOptions);
         this.meta = meta ?? {};
     }
 }
@@ -75,16 +82,16 @@ class _HttpAction extends BaseAction {
  */
 class _WebSocketAction extends BaseAction {
     executionType = ExecutionType.transport as const;
-    constructor(event: string, dependsOn: ActionDependency[] = []) {
-        super(event, dependsOn);
+    constructor(event: string, dependsOn: ActionDependency[] = [], executionOptions?: ExecutionOptions) {
+        super(event, dependsOn, executionOptions);
     }
 }
 
 class _LocalExecutedAction extends BaseAction {
     executionType = ExecutionType.local as const;
     payload: ServerEvent;
-    constructor(payload: ServerEvent) {
-        super("local_event", []);
+    constructor(payload: ServerEvent, executionOptions?: ExecutionOptions) {
+        super("local_event", [], executionOptions);
         this.payload = payload;
     }
 }
@@ -92,25 +99,25 @@ class _LocalExecutedAction extends BaseAction {
 class _ScriptAction extends BaseAction {
     executionType = ExecutionType.script as const;
     script: Script;
-    constructor(event: string, script: Script, dependsOn: ActionDependency[] = []) {
-        super(event, dependsOn);
+    constructor(event: string, script: Script, dependsOn: ActionDependency[] = [], executionOptions?: ExecutionOptions) {
+        super(event, dependsOn, executionOptions);
         this.script = script;
     }
 }
 
-export const HttpAction = (event: string, meta: HttpActionMetainfo, dependsOn?: ActionDependency[]) => {
-    return new _HttpAction(event, meta, dependsOn);
+export const HttpAction = (event: string, meta: HttpActionMetainfo, dependsOn?: ActionDependency[], executionOptions?: ExecutionOptions) => {
+    return new _HttpAction(event, meta, dependsOn, executionOptions);
 }
 
-export const LocalExecutedAction = (payload: ServerEvent) => {
-    return new _LocalExecutedAction(payload);
+export const LocalExecutedAction = (payload: ServerEvent, executionOptions?: ExecutionOptions) => {
+    return new _LocalExecutedAction(payload, executionOptions);
 }
 
 
-export const WebSocketAction = (event: string, dependsOn: ActionDependency[] = []) => {
-    return new _WebSocketAction(event, dependsOn);
+export const WebSocketAction = (event: string, dependsOn: ActionDependency[] = [], executionOptions?: ExecutionOptions) => {
+    return new _WebSocketAction(event, dependsOn, executionOptions);
 }
 
-export const ScriptAction = (event: string, script: Script, dependsOn: ActionDependency[] = []) => {
-    return new _ScriptAction(event, script, dependsOn);
+export const ScriptAction = (event: string, script: Script, dependsOn: ActionDependency[] = [], executionOptions?: ExecutionOptions) => {
+    return new _ScriptAction(event, script, dependsOn, executionOptions);
 }
